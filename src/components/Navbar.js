c0d2aa04ea5c54fea72244d7d3a717f264f7ac5b
Fredrik Wictorsson <wictorsson@gmail.com>
2023-09-07 12:28:10 +0200

    add icons for menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Linkbutton from "./Linkbutton";
 import NavlinkItem from "./NavlinkItem";
 import Searchbox from "./Searchbox";
+import { ReactComponent as CloseOutline } from "../assets/close-outline.svg";
+import { ReactComponent as MenuOutline } from "../assets/menu-outline.svg";
 
 export default function Navbar() {
   const [openMenu, setOpenMenu] = useState(false);
@@ -9,21 +11,27 @@ export default function Navbar() {
     setOpenMenu(false);
   };
   return (
-    <div className="relative bg-slate-50 flex justify-between items-center p-5 gap-2 text-gray-600">
+    <div className="fixed bg-slate-50 flex justify-between items-center p-5 gap-2 text-gray-600 w-full">
       <div
         onClick={() => setOpenMenu(!openMenu)}
         className="cursor-pointer md:hidden"
       >
-        <div>{openMenu ? "❎" : "🟰"}</div>
+        <div>
+          {openMenu ? (
+            <CloseOutline alt="close" className="w-12" />
+          ) : (
+            <MenuOutline alt="open" className="w-12" />
+          )}
+        </div>
       </div>
       <ul
-        className={`bg-slate-50 list-none md:flex md:gap-5 p-8 md:p-0 absolute top-full left-0 md:static md:left-auto 
-      md:top-auto md:pl-0 rounded ${
-        openMenu
-          ? "transition-all duration-200 ease-in opacity-100"
-          : "md:opacity-100 opacity-0"
-      }`}
+        className={`bg-slate-50 list-none md:flex md:gap-5 p-8 md:p-0 absolute top-full left-0 md:static md:left-auto md:top-auto md:pl-0 rounded ${
+          openMenu
+            ? "transition-all duration-300 ease-in opacity-100"
+            : "md:opacity-100 opacity-0"
+        }`}
       >
+        {/* Menu items */}
         <NavlinkItem onClick={handleMenuClick} title="Startseite" route="/" />
         <NavlinkItem
           onClick={handleMenuClick}
@@ -42,16 +50,17 @@ export default function Navbar() {
           route="/about"
         />
       </ul>
-      <div>WeTo</div>
-      <ul className="list-none flex gap-4 items-center">
-        <li>
+      <div className="text-sm">WeTo</div>{" "}
+      {/* Smaller font size on smaller screens */}
+      <ul className="list-none flex gap-2 items-center text-xs lg:text-base">
+        <li className="absolute left-1/2 -translate-x-1/2 translate-y-14 md:translate-y-0 md:-translate-x-0 md:left-0 md:relative  ">
           <Searchbox title="Gruppe Suchen" />
         </li>
 
-        <li className="md:my-0  ">
+        <li className="md:my-0">
           <Linkbutton title="Anmelden" route="/" />
         </li>
-        <li className="md:my-0 ">
+        <li className="md:my-0">
           <Linkbutton
             title="Mitglied Werden"
             route="/"
--- src/components/NavlinkItem.js
+++ src/components/NavlinkItem.js
@@ -2,8 +2,11 @@ import { NavLink as Link } from "react-router-dom";
 
 export default function NavlinkItem({ onClick, title, route }) {
   return (
-    <li onClick={onClick} className="md:my-0 mb-7 md:text-base text-lg">
-      <Link to={route} className="hover:text-primarybutton">
+    <li
+      onClick={onClick}
+      className="md:my-0 mb-7 lg:text-base md:text-xs text-lg"
+    >
+      <Link to={route} className="hover:text-primarybutton whitespace-nowrap">
         {title}
       </Link>
     </li>
