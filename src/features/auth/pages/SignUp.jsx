b1b380f598776392201b1fb8dbdd33e5db02fa47
Fredrik Wictorsson <wictorsson@gmail.com>
2023-12-07 16:37:59 +0100

    add primary buttons to signup

--- src/features/auth/pages/SignUp.jsx
+++ src/features/auth/pages/SignUp.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { createAction } from "@reduxjs/toolkit";
-
 import AuthForm from "../components/AuthForm";
-
 import logoSvg from "../../../assets/imgLogos/logoNoBg.svg";
 import useMultistepHook from "../../../util/hooks/multistepHook";
 import getFunnelSteps from "../util/getFunnelSteps";
 import StepIndicator from "../components/RegStepper";
 import { setAuthToken } from "../authSlice";
+import PrimaryButton from "../../../components/Buttons/PrimaryButton";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 
 const populateAuthForm = createAction("populateAuthForm");
 
@@ -55,10 +55,10 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex w-full h-full md:justify-center mt-5 md:mt-10 lg:mt-12 pb-24 bg-BG_PRIMARY md:bg-BG_GRAY">
+    <div className="flex w-full h-full md:justify-center  md:mt-[4vh] pb-24 bg-BG_PRIMARY md:bg-BG_GRAY">
       <div
-        className="min-h-[85vh] lg:min-h-[85vh] md:w-3/4 lg:w-1/2 
-      px-4 rounded md:shadow-md bg-BG_PRIMARY md:px-8 lg:px-28 pt-8 "
+        className="min-h-[90vh] lg:min-h-[90vh] md:w-3/4 lg:w-1/2 
+      px-4 rounded md:shadow-md bg-BG_PRIMARY md:px-8 lg:px-28 pt-4 "
       >
         <div className="pb-3 flex justify-center">
           <img src={logoSvg} alt="logo" className="lg:w-40 w-28" />
@@ -81,12 +81,33 @@ const SignUp = () => {
                 onSubmit={handleSubmit}
                 groupId={joinGroupId}
               >
-                {!isFirstStep && step.goBackOption && (
-                  <button onClick={back}>Back</button>
-                )}
-                <button type="submit">
-                  {isLastStep ? "Submit" : "Weiter"}
-                </button>
+                <div className="flex justify-end gap-8 mt-8">
+                  {!isFirstStep && step.goBackOption && (
+                    <PrimaryButton
+                      type={"button"}
+                      handleButtonClick={back}
+                      isInversed={true}
+                      isLarge={true}
+                    >
+                      <div className="flex items-center ">
+                        <BsArrowLeft
+                          className="w-5 mr-3 text-PURPLE_PRIMARY"
+                          size={18}
+                        />
+                        Zurück
+                      </div>
+                    </PrimaryButton>
+                  )}
+                  <PrimaryButton type={"submit"} isLarge={true}>
+                    <div className="flex items-center ">
+                      {isLastStep ? "Submit" : "Weiter"}
+                      <BsArrowRight
+                        className="w-5 ml-3 text-BG_PRIMARY"
+                        size={18}
+                      />
+                    </div>
+                  </PrimaryButton>
+                </div>
               </AuthForm>
             }
           </div>
