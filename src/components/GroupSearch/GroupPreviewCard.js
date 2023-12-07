787068b2b5c9e4174c06403c5c2f919b5f0619fa
Fritz Meyer <fritz.meyer@code.berlin>
2023-12-07 20:47:05 +0100

    simplified GroupPreviewCard.js

--- src/components/GroupSearch/GroupPreviewCard.js
+++ src/components/GroupSearch/GroupPreviewCard.js
@@ -1,50 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import PrimaryButton from "../Buttons/PrimaryButton";
 import GroupOverviewContent from "../GroupDetails/GroupOverviewContent";
 import { motion } from "framer-motion";
-import { Image } from "cloudinary-react";
 import placeholderImage from "../../assets/placeholderImage.jpg";
 
-const fadeInVariants = {
-  hidden: { opacity: 0 },
-  visible: { opacity: 1 },
-  exit: { opacity: 0 },
-};
-
 const GroupPreviewCard = ({ group }) => {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
-  const handleImageLoad = () => {
-    setImageLoaded(true);
-  };
-
   return (
     <div className="flex flex-col border rounded-md shadow-md h-full ">
       <div className="w-full">
-        {!imageLoaded && (
-          <motion.img
-            src={placeholderImage}
-            alt="Placeholder"
-            className="rounded-md object-cover filter blur-md"
-          />
-        )}
-        <motion.div
-          initial="hidden"
-          animate={imageLoaded ? "visible" : "hidden"}
-          exit="exit"
-          variants={fadeInVariants}
-          transition={{ duration: 0.3 }}
-          className="rounded-t-md overflow-hidden"
-        >
-          <Image
-            cloudName="di8ujuqae"
-            publicId={group.image}
-            className="object-cover w-full"
-            loading="lazy"
-            onLoad={handleImageLoad}
-          />
-        </motion.div>
+        <motion.img
+          src={placeholderImage}
+          alt="Placeholder"
+          className="rounded-md object-cover filter blur-md"
+        />
       </div>
 
       <div className="flex flex-col justify-between px-4 h-full">
