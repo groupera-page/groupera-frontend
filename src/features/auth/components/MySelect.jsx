b082aa3fbb2873e9a766f2d741f624124fefbaa7
Fredrik Wictorsson <wictorsson@gmail.com>
2023-12-08 16:02:15 +0100

    fix styling checkboxes

--- src/features/auth/components/MySelect.jsx
+++ src/features/auth/components/MySelect.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { AiOutlineWarning } from "react-icons/ai";
+import { BsCheckLg } from "react-icons/bs";
 
 const MySelect = ({
   input,
@@ -49,6 +50,7 @@ const MySelect = ({
           } rounded-md text-base py-3 px-4 flex items-center gap-4 hover:shadow-md`}
         >
           <div className="pr-8">{opt.label}</div>
+
           <input
             type={isMultiSelect() ? "checkbox" : "radio"}
             id={`opt-${idx}`}
@@ -60,8 +62,18 @@ const MySelect = ({
                 : input.value === opt.value
             }
             onChange={(event) => handleOnChange(event, opt)}
-            className="mx-1 absolute end-1 md:end-5 h-5 w-5 appearance-none rounded-full border before:pointer-events-none before:rounded-full checked:border-PURPLE_PRIMARY hover:cursor-pointer hover:before:opacity-[0.07] checked:border-4 focus:outline-none focus:ring-0 border-BORDER_PRIMARY "
+            className={`mx-1 absolute end-1 md:end-5 h-5 w-5 appearance-none bg-BG_GRAY ${
+              isMultiSelect()
+                ? "checked:bg-PURPLE_PRIMARY rounded-sm "
+                : " rounded-full hover:cursor-pointer hover:before:opacity-[0.07] checked:border-4 focus:outline-none focus:ring-0"
+            } border before:pointer-events-none before:rounded-full checked:border-PURPLE_PRIMARY  border-BORDER_PRIMARY `}
           />
+          {isMultiSelect() && (
+            <BsCheckLg
+              className={`absolute end-2 md:end-6 rounded-md text-BG_GRAY`}
+              size={20}
+            />
+          )}
         </label>
       ))}
 
