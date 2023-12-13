01f20fbc511d7548a7f748a64458ea694aba2a5f
Fredrik Wictorsson <wictorsson@gmail.com>
2023-12-13 11:31:00 +0100

    set max width on group overview cards and break words on group detail card

--- src/components/GroupSearch/GroupPreviewCard.js
+++ src/components/GroupSearch/GroupPreviewCard.js
@@ -8,10 +8,10 @@ const GroupPreviewCard = ({ group }) => {
   return (
     <div className="flex flex-col border rounded-md shadow-md h-full ">
       <div className="w-full">
-        <LazyLoadImg img={group.img}/>
+        <LazyLoadImg img={group.img} />
       </div>
 
-      <div className="flex flex-col justify-between px-4 h-full">
+      <div className="flex flex-col justify-between px-4 h-full max-w-sm">
         <GroupOverviewContent clamp={true} group={group} />
         <div className="my-4">
           <Link to={`/groups/${group.id}`}>
@@ -23,6 +23,6 @@ const GroupPreviewCard = ({ group }) => {
       </div>
     </div>
   );
-}
+};
 
-export default GroupPreviewCard
+export default GroupPreviewCard;
