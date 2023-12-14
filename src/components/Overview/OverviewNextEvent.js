252094776870cb0a743215ec88a5db2ed5a2a66a
Fredrik Wictorsson <wictorsson@gmail.com>
2023-12-14 11:31:29 +0100

    fix overflow styling - desktop on next event card

--- src/components/Overview/OverviewNextEvent.js
+++ src/components/Overview/OverviewNextEvent.js
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
 import PrimaryButton from "../Buttons/PrimaryButton";
-import getFormatedDate, {isNowBetween} from "../../util/formatMeetingDate";
+import getFormatedDate, { isNowBetween } from "../../util/formatMeetingDate";
 
 const OverviewNextEvent = ({ nextEvent }) => {
   const [joinEventWarning, setJoinEventWarning] = useState(false);
 
   const handleButtonClick = () => {
     // setJoinEventWarning((prev) => !prev);
-    if (!nextEvent) return
-    const endTime = new Date(nextEvent.meeting.startDate)
-    endTime.setMinutes(new Date(endTime).getMinutes() + nextEvent.meeting.duration)
+    if (!nextEvent) return;
+    const endTime = new Date(nextEvent.meeting.startDate);
+    endTime.setMinutes(
+      new Date(endTime).getMinutes() + nextEvent.meeting.duration
+    );
 
     if (!isNowBetween(new Date(nextEvent.meeting.startDate), endTime)) {
       setJoinEventWarning(true);
@@ -32,27 +34,27 @@ const OverviewNextEvent = ({ nextEvent }) => {
     <div>
       <div className="flex flex-col lg:flex-row w-full justify-between">
         <div className="flex flex-col pt-2">
-          {
-            !nextEvent ? (
-              <div className="flex justify-center">
-                <p className="paragraph-sm text-TEXT_PRIMARY lg:mt-2">Kein Termin geplant</p>
+          {!nextEvent ? (
+            <div className="flex justify-center">
+              <p className="paragraph-sm text-TEXT_PRIMARY lg:mt-2">
+                Kein Termin geplant
+              </p>
+            </div>
+          ) : (
+            <div className="flex flex-col justify-center ">
+              <div className="flex flex-col mb-2 ">
+                <p className="paragraph-md font-semibold line-clamp-1 break-words">
+                  {nextEvent.group.name}
+                </p>
+                <p className="paragraph-sm text-TEXT_PRIMARY lg:mt-2">
+                  {getFormatedDate(nextEvent.meeting)}
+                </p>
               </div>
-            ) : (
-              <div className="flex flex-col justify-center ">
-                <div className="flex flex-col mb-2 ">
-                  <p className="paragraph-md font-semibold line-clamp-1 break-words">
-                    {nextEvent.group.name}
-                  </p>
-                  <p className="paragraph-sm text-TEXT_PRIMARY lg:mt-2">
-                    {getFormatedDate(nextEvent.meeting)}
-                  </p>
-                </div>
-              </div>
-            )
-          }
+            </div>
+          )}
         </div>
 
-        <div className="w-full lg:w-1/4 pb-5 lg:p-2.5 lg:px-4">
+        <div className="w-full lg:w-1/2 pb-5 lg:p-2.5 lg:px-4">
           {nextEvent && (
             <div className="flex flex-row lg:flex-col h-full justify-center items-end w-full gap-3">
               <div className="">
